dc0d04b7b344ab84db6557f42ecd2509b5828934
Dani Crosby <dani.crosby1111@gmail.com>
2021-03-04 16:52:24 -0600

    added navBar and profile page skeleton, needs functionality

---
+++ src/javascripts/components/domBuilder.js
@@ -0,0 +1,7 @@
+const domBuilder = () => {
+  document.querySelector('#app').innerHTML = `
+  <div id="navigation"></div>
+`;
+};
+
+export default domBuilder;
---
+++ src/javascripts/components/navBar.js
@@ -0,0 +1,57 @@
+const navBar = () => {
+  document.querySelector('#navigation').innerHTML = `
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" id="nav-container">
+
+  <div><a href=""><img src="https://cdn4.iconfinder.com/data/icons/social-media-flat-7/64/Social-media_Pinterest-512.png" style="width: 1.5em; margin-left: 1em; margin-right: .5em"></div></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span></button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+       <li class="nav-item active">
+        <a class="nav-link" href="#">Today <span class="sr-only">#</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Following <span class="sr-only">#</span></a>
+      </li>
+    </ul>
+    
+    <div class="input-group">
+      <input type="text" class="form-control w-75 ml-3" aria-label="Text input with dropdown button" placeholder="search">
+    <div class="dropdown show">
+      <a class="btn btn-secondary dropdown-toggle mr-3" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All Pins</a>
+  
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+      <a class="dropdown-item" href="#">All Pins</a>
+      <a class="dropdown-item" href="#">All Boards</a>
+    </div>
+  </div>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <div><a href=""><img src="https://cdn1.iconfinder.com/data/icons/app-user-interface-glyph/64/notification_user_interface_app_alarm_bell-512.png" style="width: 1.5em; margin-left: 1em; margin-right: .5em"></div></a>
+      </li>
+      
+      <li class="nav-item active">
+        <div><a href=""><img src="https://cdn4.iconfinder.com/data/icons/music-ui-solid-24px/24/chat_bubble_notification_message-2-512.png" style="width: 1.5em; margin-left: 1em; margin-right: .5em"></div></a>
+      </li>
+      
+      <li class="nav-item active">
+        <div><a href=""><img src="https://cdn3.iconfinder.com/data/icons/social-messaging-productivity-6/128/profile-circle2-512.png" style="width: 1.5em; margin-left: 1em; margin-right: .5em"></div></a>
+      </li>
+
+      <li class="nav-item active">
+        <div><a href=""><img src="https://cdn1.iconfinder.com/data/icons/flat-and-simple-part-1/128/arrow_down-512.png" style="width: 1.5em; margin-left: 1em; margin-right: .5em"></div></a>
+      </li>
+    
+    </ul>
+
+  </div>
+</nav>`;
+};
+
+export default navBar;
