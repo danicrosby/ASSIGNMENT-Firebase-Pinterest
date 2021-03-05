719a39434db4be55ecbbaecd82f177bb2f478228
Dani Crosby <dani.crosby1111@gmail.com>
2021-03-04 21:59:43 -0600

    firebase, nav, auth, need more functionality

--- src/javascripts/views/startApp.js
+++ src/javascripts/views/startApp.js
@@ -1,9 +1,15 @@
 import domBuilder from '../components/domBuilder';
+import loginButton from '../components/loginButton';
+import logoutButton from '../components/logoutButton';
 import navBar from '../components/navBar';
+import profilePage from '../components/profilePage';
 
 const startApp = () => {
   domBuilder();
+  loginButton();
   navBar();
+  profilePage();
+  logoutButton();
 };
 
 export default startApp;
