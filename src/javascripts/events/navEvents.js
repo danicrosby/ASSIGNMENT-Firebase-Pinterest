1a658181a75ef932329736f0754fd2f4273c9d96
Dani Crosby <dani.crosby1111@gmail.com>
2021-03-13 19:28:06 -0600

    login and nav showing

--- src/javascripts/events/navEvents.js
+++ src/javascripts/events/navEvents.js
@@ -1,33 +1,5 @@
-// PINS = pins //
-// BOARDS = Authors//
-
-import { emptyBoards, showBoards } from '../components/boards';
-import { emptyPins, showPins } from '../components/pins';
-import { getBoards } from '../helpers/data/boardData';
-import { getPins } from '../helpers/data/pinData';
-
 const navigationEvents = () => {
-  // ALL PINS
-  document.querySelector('#all-pins').addEventListener('click', () => {
-    getPins().then((pinsArray) => {
-      if (pinsArray.length) {
-        showPins(pinsArray);
-      } else {
-        emptyPins();
-      }
-    });
-  });
 
-  // ALL BOARDS
-  document.querySelector('#all-boards').addEventListener('click', () => {
-    getBoards().then((boardArray) => {
-      if (boardArray.length) {
-        showBoards(boardArray);
-      } else {
-        emptyBoards();
-      }
-    });
-  });
 };
 
 export default navigationEvents;
